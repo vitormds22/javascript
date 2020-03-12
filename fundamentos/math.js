f297681d2590597857e6a897bf0cb44d749e005c
Vitor Machado <vitormds22@gmail.com>
2020-03-12 20:33:07 -0300

    About numbers

---
+++ fundamentos/math.js
@@ -0,0 +1,5 @@
+const raio = 5.6
+const area = Math.PI * Math.pow(raio, 2) //PI constante e Pow elevado
+
+console.log(area)
+console.log(typeof Math)
---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,11 @@
+console.log(7 / 0)
+
+console.log("10" / 2) //Ele entende que a string é um inteiro e realiza a operção
+
+console.log("Show!" * 2) //NaN
+
+console.log(0.1 + 0.7) // As operações terão uma certa imprecisão, por isso é mais rapido compilar
+
+// console.log(10.toString())
+
+console.log((10.345).toFixed(2))
