83de9905fb86340a339ec472a4602afeb791aa85
Vitor Machado <vitormds22@gmail.com>
2020-03-13 20:08:54 -0300

    End of about object

--- fundamentos/objeto.js
+++ fundamentos/objeto.js
@@ -1,5 +1,19 @@
 const prod1 = {} //Chave valor
 prod1.nome = "Celular Ultra Mega"  //Pode criar valores dentro do objeto dessa forma
 prod1.preco = 4998
+// prod1['Desconto legal'] = 0.40 // Evitar atributos com espaço
 
 console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa',
+    preco: 79.80,
+    obj: {
+        param: 1,
+        obj: {
+            param2: 2
+        }
+    }
+}
+
+console.log(prod2)
