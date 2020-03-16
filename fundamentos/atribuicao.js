c318424e09cbfd7286e3758e5e0c49f1863a9bf1
Vitor Machado <vitormds22@gmail.com>
2020-03-16 15:57:44 -0300

    About dot notation and atribution

---
+++ fundamentos/atribuicao.js
@@ -0,0 +1,17 @@
+const a = 7
+let b = 3 
+
+b += a //Acrescentando o valor de a ao valor de b => b= b + a 
+console.log(b)
+
+b -= 4 //b = b - 4
+console.log(b)
+
+b *= 2 // b = b * 2
+console.log(b)
+
+b /= 2 // b = b / 2
+console.log(b)
+
+b %= 2 // b = b % 2
+console.log(b)
--- fundamentos/notacaoPonto.js
+++ fundamentos/notacaoPonto.js
@@ -1,1 +1,23 @@
-notacaoPonto.js
+//Math é um objeto e acessamos o metodo ceil para arredondar para cima
+console.log(Math.ceil(6.1)) //A notação ponto já existe aqui mesmo nessa linha
+
+const obj1 = {}
+obj1.nome = "Bola"
+
+console.log(obj1.nome)
+
+function Obj(nome){
+    //this faz ficar visivel fora do escopo da função
+    this.nome = nome
+    this.exec = function(){
+        console.log('Exec...')
+    }
+}
+
+const obj2 = new Obj('Cadeira')
+const obj3 = new Obj('Mesa')
+
+//Com o ponto pode se acessar tanto atributos como funções de um objeto
+console.log(obj2.nome)
+console.log(obj3.nome)
+obj3.exec()
