d1c977cb5aaf7af7cd18c91a5383c01f69041c8a
Vitor Machado <vitormds22@gmail.com>
2020-03-21 17:28:57 -0300

    About arrow functions

---
+++ functions/arrowFunction3.js
@@ -0,0 +1,21 @@
+let comparaComThis = function(param){
+    console.log(this === param)
+}
+
+comparaComThis(global)
+comparaComThis(this)//Nesse caso o this não é o this
+
+const obj = {}
+
+comparaComThis = comparaComThis.bind(obj)
+
+comparaComThis(global)
+comparaComThis(obj)
+
+let comparaComThisArrow = param => console.log(this === param)
+comparaComThisArrow(global)
+comparaComThisArrow(module.exports)
+
+comparaComThisArrow = comparaComThisArrow.bind(obj)
+comparaComThisArrow(obj)
+comparaComThisArrow(module.exports)
