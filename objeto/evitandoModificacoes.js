ef5db379b5e780f7b5320c4cbc74a4716a27829d
Vitor Machado <vitormds22@gmail.com>
2020-03-30 14:06:09 -0300

    About modifications in objects

--- objeto/evitandoModificacoes.js
+++ objeto/evitandoModificacoes.js
@@ -1,1 +1,24 @@
-evitandoModificacoes.js
+// Object.preventExtensions - Pode alterar, pode excluir, não pode incluir
+const produto = Object.preventExtensions({
+    nome: 'Qualquer', preco: 1.99, tag: 'promoção'
+})
+
+console.log('Extensível: ', Object.isExtensible(produto))
+
+produto.nome = 'Borracha'
+produto.desc = 'Borracha escolar branca'
+delete produto.tag
+console.log(produto)
+
+//Object.seal - Não adiciona, não exclui mas pode modificar
+const pessoa = { nome: 'Juliana', idade: 35 }
+Object.seal(pessoa)
+
+console.log('Selado: ', Object.isSealed(pessoa))
+
+pessoa.sobrenome = 'Silva'
+delete pessoa.nome
+pessoa.idade = 29
+console.log(pessoa)
+
+//Object.freeze = selado + valores constantes => Não modifica em nada
