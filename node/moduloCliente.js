ea4ebea2a5fab1071b60bcaf62ba8e54102c5a49
Vitor Machado <vitormds22@gmail.com>
2020-04-05 20:31:15 -0300

    Initializing node, modules e require

---
+++ node/moduloCliente.js
@@ -0,0 +1,7 @@
+const moduloA = require('./moduloA')
+const moduloB= require('./moduloB')
+
+console.log(moduloA.ola, moduloA.bemVindo, moduloA.ateLogo)
+console.log(moduloA)
+
+console.log(moduloB, moduloB.boaNoite())
