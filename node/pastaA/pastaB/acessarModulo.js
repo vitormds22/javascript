1fb1feba2057b0de732d96357e929ab5db73a622
Vitor Machado <vitormds22@gmail.com>
2020-04-05 20:40:39 -0300

    About require

--- node/pastaA/pastaB/acessarModulo.js
+++ node/pastaA/pastaB/acessarModulo.js
@@ -1,4 +1,12 @@
 const moduloA = require('../../moduloA')
 
-
 console.log(moduloA)
+
+const c = require('./pastaC')
+console.log(c.ola2)
+
+// const http = require('http')
+// http.createServer((req, res) => {
+//     res.write('Bom dia')
+//     res.end()
+// }).listen(8080)
